2a14bae882c43a70a494c0285099ad7762ebe154
jgfzm4hvfs-cyber <ozzemxim@gmail.com>
2026-02-14 18:57:02 +0100

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -3,12 +3,12 @@
   This is intentionally file-based (not localStorage).
 */
 window.DISCIPLINE_OS_CLOUD_CONFIG = {
-  enabled: false,
-  webAppUrl: "",
+  enabled: true,
+  webAppUrl: "https://script.google.com/macros/s/AKfycbycegaCo25lA5XuQbUyWFtm5k1FzlubvOuaUBMr-nGO7lVl3_WMySMvA4uaDdOn3O_v/exec",
   userId: "default",
 };
 
 window.DISCIPLINE_OS_AUTH_CONFIG = {
-  googleClientId: "",
-  allowedEmail: "",
+  googleClientId: "1085915103307-rrnbhhsi7d9melec9j6705fd400jaa5e.apps.googleusercontent.com",
+  allowedEmail: "ozzemxim@gmail.com",
 };
